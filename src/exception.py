14292c72423b9087ff5de2a5ba893ea370366b19
sumama <sumamakhan761>
2024-07-13 13:07:38 +0530

    ingesstion

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,8 @@
+# interpreter is a computer program that converts each high-level program statement into machine code
+
 import sys
 from src.logger import logging
+
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -8,8 +11,6 @@ def error_message_detail(error,error_detail:sys):
      file_name,exc_tb.tb_lineno,str(error))
 
     return error_message
-
-    
 
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
